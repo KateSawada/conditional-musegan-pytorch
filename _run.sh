9797b8ade5448e55dfb15cb1b5449f539622fa79
KateSawada <sawapy630@gmail.com>
2022-11-01 16:23:58 +0900

    バックグラウンドジョブスクリプト修正 #8

--- _run.sh
+++ _run.sh
@@ -3,5 +3,5 @@
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
-source /data/group1/${USER}/data/musegan-pytorch/venv/bin/activate
-python -u python train.py -c config/train.yml config/base.yml -m model/furo/
+source venv/bin/activate
+python train.py -c config/train.yml config/base.yml -m model/furo/
