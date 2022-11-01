04fb5507c6cc73ec1f54d45aeef5a22aa7ca986d
KateSawada <sawapy630@gmail.com>
2022-11-01 16:16:44 +0900

    複数GPU対応 #8

--- _run.sh
+++ _run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
 source /data/group1/${USER}/data/musegan-pytorch/venv/bin/activate
 python -u python train.py -c config/train.yml config/base.yml -m model/furo/
