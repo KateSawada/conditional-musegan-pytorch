ed0336e08d02952479562ef39338722902b9caa2
KateSawada <sawapy630@gmail.com>
2022-11-27 16:55:28 +0900

    不老スクリプト編集 #21

--- _run.sh
+++ _run.sh
@@ -4,4 +4,4 @@ export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
 source venv/bin/activate
-python train.py -c config/train.yml config/base.yml -m model/furo/
+python train.py -c config/train_con.yml config/base_con.yml -m conditioning_middle_model_64
